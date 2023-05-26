5bf6328c2ae441ce6d77c9a7598b764f6a693c50
Yubeen <lybin71@gmail.com>
2023-05-26 10:41:08 +0100

    1st commit

---
+++ london-blog-cloning/app/admin/page.tsx
@@ -0,0 +1,9 @@
+"use clint";
+import config from "@/sanity.config";
+import { NextStudio } from "next-sanity/studio";
+
+const AdminPage = () => {
+  return <NextStudio config={config} />;
+};
+
+export default AdminPage;
