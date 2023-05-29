779feaf861e52189e96989b209775947b49e8b69
Yubeen <lybin71@gmail.com>
2023-05-29 10:58:11 +0100

    4th commit

--- london-blog-cloning/app/projects/[project]/page.tsx
+++ london-blog-cloning/app/projects/[project]/page.tsx
@@ -1,13 +1,13 @@
 type Props = {
-params: {project : string}
+params: {project : string} //이해 필요
 }
 
 import { getProject } from "@/sanity/sanity-utils";
 
-export default async function Project( {params} : Props ){
-
-    const slug = params.project; //params 객체의 project key value를 slug로 함
-    const project = await getProject(slug);     //이해함 (slug를 이 파일명으로 가지고 오는 것)
+export default async function Project( {params} : Props ){ //이해함 {params를 변수로 받을거고, 이 변수의 type은 Props}
+ 
+    const slug = params.project; //이해함 (params 객체의 project??? 값을 slug로 함)
+    const project = await getProject(slug);     //이해함 (project를 slug를 이 파일명으로 가지고 오는 것)
 
     return <div>{project.name}</div>
 }
