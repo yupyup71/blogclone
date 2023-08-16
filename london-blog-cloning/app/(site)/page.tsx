9ab72d05e665ca315af0e1a77f271b3485c0d1d7
Yubeen <lybin71@gmail.com>
2023-08-16 18:42:05 +0100

    ektl

--- london-blog-cloning/app/(site)/page.tsx
+++ london-blog-cloning/app/(site)/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
       <h1 className="text-7xl font-extrabold">
         Hello I&apos;m{" "}
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
-          Nicolas !
+          Nicolas!
         </span>
       </h1>
 
